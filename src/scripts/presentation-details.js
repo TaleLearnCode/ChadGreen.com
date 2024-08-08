83ce8607239b97f2797a0a6bc0fb1f52a57daad0
Martine Dowden <martine@andromeda16.com>
2024-08-08 14:08:22 -0400

    mobile accordion

--- src/scripts/presentation-details.js
+++ src/scripts/presentation-details.js
@@ -1,10 +1,29 @@
 const tabs = Array.from(document.querySelectorAll('.tab'));
+const accordion = document.querySelector('.accordion')
+const accordionPanels = Array.from(accordion.querySelectorAll('.panel'))
 const nav = document.getElementById('tabsNav')
 let previousWasMobile = false
-const minWindowWidth = 455
+const minWindowWidth = 455;
 
-window.addEventListener('resize', handleTabResize)
-handleTabResize()
+(function() {
+  'use strict'
+  window.addEventListener('resize', () => {
+    if (!isMobile) return;
+    accordionPanels.forEach(panel => {
+      handleAccordionPanel(panel, Array.from(panel.classList).includes('expanded')) 
+    })
+  })
+})();
+
+function handleAccordionPanel(panel, opening) {
+  const body = panel.querySelector('.panel-body')
+  if (opening) {
+    const panelHeight = body.offsetHeight
+    panel.style.maxHeight = `calc(2.5rem + ${panelHeight}px)`
+  } else {
+    panel.style.maxHeight = '2.5rem'
+  }
+}
 
 function selectTab(tab, event) {
   const navItems = Array.from(nav.querySelectorAll('li'))
@@ -14,34 +33,13 @@ function selectTab(tab, event) {
   document.getElementById(tab).classList.add('selected')
   const originalNode = event.srcElement.parentElement
   originalNode.classList.add('selected')
-  
-  if (!isMobile()) { return }
-  const clone = originalNode.cloneNode(true)
-  nav.querySelector('ul').appendChild(clone)
-  originalNode.remove()
 }
 
-function handleTabResize() {
-  console.log('resize')
-  if ((isMobile() && previousWasMobile) || (!isMobile() && !previousWasMobile)) { return }
-  previousWasMobile = isMobile()
-  const navItems = Array.from(nav.querySelectorAll('li'))
-  const ul = nav.querySelector('ul')
-  if (isMobile()) {
-    navItems.sort((a,b) => {
-      if (a.classList.contains('selected')) { return 1 }
-      if (b.classList.contains('selected')) { return -1 }
-      return Number(b.getAttribute('data-order')) - Number(a.getAttribute('data-order'))
-    })
-  } else {
-    // else desktop
-    navItems.sort((a,b) => {
-      return Number(a.getAttribute('data-order')) - Number(b.getAttribute('data-order'))
-    })
-  }
-
-  ul.innerHTML = null
-  navItems.forEach(item => ul.appendChild(item))
+function togglePanel(event) {
+  console.log(event)
+  const panel = event.srcElement.closest('.panel')
+  handleAccordionPanel(panel, !Array.from(panel.classList).includes('expanded'))
+  panel.classList.toggle('expanded')
 }
 
 function isMobile() {
